622435d9a6676e30119afb99f70cf1dc6e3da7f8
suhaybcabdulahi <sabkacade40@gmail.com>
2021-09-06 10:50:04 +0530

    TextInput with Button and with little bit of styling

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goals"
+          style={styles.input} />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'
+  },
+  input: {
+    width: '80%', borderColor: 'black', borderWidth: 1, padding: 10
+  }
 });
